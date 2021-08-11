11557de28c23c3d98e8584dd3e8e24fe02496c68
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-08-11 04:02:18 +0200

    update docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -66,8 +66,8 @@ autodoc_member_order = 'bysource'                       # https://www.sphinx-doc
 # we're uysing sphinx_autodoc_typehints
 autodoc_typehints = 'description'                       # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_typehints
 autoclass_content = 'both'                              # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
-autosummary_generate = True                             # https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html?highlight=autosummary_generate#confval-autosummary_generate
-autodoc_mock_imports = ['vapoursynth', 'vsutil']        # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
+autosummary_generate = True                             # https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html?highlight=autosummary_generate#confval-autosummary_generate  # type: ignore
+autodoc_mock_imports = ['vapoursynth', 'vsutil', 'lxml', 'vardefunc']        # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
 smartquotes = True                                      # https://www.sphinx-doc.org/en/master/usage/configuration.html?highlight=smartquotes#confval-smartquotes
 html_show_sphinx = False                                # https://www.sphinx-doc.org/en/master/usage/configuration.html?highlight=smartquotes#confval-html_show_sphinx
 pygments_style = 'sphinx'                               # https://www.sphinx-doc.org/en/master/usage/configuration.html?highlight=pygments_style#confval-pygments_style
