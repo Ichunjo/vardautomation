1b742d95fbf70e40c431afd24cb9c2bf76416d37
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-08-11 03:53:54 +0200

    add metadata file

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-import setuptools
+from setuptools import setup
+from distutils.util import convert_path
+
+meta = {}
+exec(open(convert_path('vardautomation/_metadata.py')).read(), meta)
 
 
 with open('README.md') as fh:
@@ -8,13 +12,12 @@ with open("requirements.txt") as fh:
     install_requires = fh.read()
 
 NAME = 'vardautomation'
-VERSION = '0.3.0'
 
-setuptools.setup(
+setup(
     name=NAME,
-    version=VERSION,
-    author='Vardë',
-    author_email='ichunjo.le.terrible@gmail.com',
+    version=meta['__version__'],
+    author=meta['__author__'].split()[0],
+    author_email=meta['__author__'].split()[1][1:-1],
     description='Automatisation tools',
     long_description=long_description,
     long_description_content_type='text/markdown',
---
+++ vardautomation/_metadata.py
@@ -0,0 +1,2 @@
+__author__ = 'Ichunjo <ichunjo.le.terrible@gmail.com'
+__version__ = '0.3.0'
