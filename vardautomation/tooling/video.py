2c4f3f2c90112cab446a5ed04f9588e4787aeaae
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-10-16 14:25:31 +0200

    tooling video: fix progress func error

--- vardautomation/tooling/video.py
+++ vardautomation/tooling/video.py
@@ -50,7 +50,7 @@ class VideoEncoder(Tool):
     More informations http://www.vapoursynth.com/doc/pythonreference.html#VideoNode.output
     """
 
-    progress_update: Optional[UpdateFunc] = progress_update_func
+    progress_update: Optional[UpdateFunc] = None
     """Progress update function to be used in `vapoursynth.VideoNode.output`"""
 
     prefetch: int = 0
@@ -187,8 +187,10 @@ class X265Encoder(VideoLanEncoder):
 
     @copy_docstring_from(VideoLanEncoder.__init__)
     def __init__(self, settings: Union[AnyPath, List[str], Dict[str, Any]], /,
-                 zones: Optional[Dict[Tuple[int, int], Dict[str, Any]]] = None) -> None:
+                 zones: Optional[Dict[Tuple[int, int], Dict[str, Any]]] = None,
+                 progress_update: Optional[UpdateFunc] = progress_update_func) -> None:
         super().__init__(BinaryPath.x265, settings, zones)
+        self.progress_update = progress_update
 
     def set_variable(self) -> Dict[str, Any]:
         min_luma, max_luma = Properties.get_colour_range(self.params, self.clip)
@@ -200,8 +202,10 @@ class X264Encoder(VideoLanEncoder):
 
     @copy_docstring_from(VideoLanEncoder.__init__)
     def __init__(self, settings: Union[AnyPath, List[str], Dict[str, Any]], /,
-                 zones: Optional[Dict[Tuple[int, int], Dict[str, Any]]] = None) -> None:
+                 zones: Optional[Dict[Tuple[int, int], Dict[str, Any]]] = None,
+                 progress_update: Optional[UpdateFunc] = progress_update_func) -> None:
         super().__init__(BinaryPath.x264, settings, zones)
+        self.progress_update = progress_update
 
     def set_variable(self) -> Dict[str, Any]:
         return super().set_variable() | dict(csp=Properties.get_csp(self.clip))
