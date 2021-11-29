77c35c23fc2e7ccb086718e9dce366814d28a84f
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-11-29 02:16:07 +0100

    tooling: fix AttributeError

--- vardautomation/tooling/abstract.py
+++ vardautomation/tooling/abstract.py
@@ -57,6 +57,7 @@ class Tool(ABC):
         """
         self.binary = VPath(binary)
         self.settings = settings
+        self.params = []
         super().__init__()
 
     @abstractmethod
@@ -68,7 +69,6 @@ class Tool(ABC):
         """Set variables in the settings"""
 
     def _get_settings(self) -> None:
-        self.params = []
         if isinstance(self.settings, dict):
             for k, v in self.settings.items():
                 self.params += [k] + ([str(v)] if v else [])
