0ec56e0a862a33d8c3e25fb9fc4de191256fa885
Vardë <ichunjo.le.terrible@gmail.com>
2026-08-31 23:14:52 +0200

    format

--- tests/test_file_info.py
+++ tests/test_file_info.py
@@ -1,33 +1,27 @@
-from typing import List
 from vardautomation import FileInfo
 
-
-FILEINFO_ATTR: List[str] = [
-    'path',
-    'path_without_ext',
-    'work_filename',
-    'idx',
-    'preset',
-    'name',
-    'workdir',
-    'a_src',
-    'a_src_cut',
-    'a_enc_cut',
-    'chapter',
-    'clip',
-    '_trims_or_dfs',
-    'clip_cut',
-    'name_clip_output',
-    'name_file_final',
-    'name_clip_output_lossless',
-    'do_lossless',
-    'qpfile',
-    'do_qpfile'
+FILEINFO_ATTR: list[str] = [
+    "workdir",
+    "path",
+    "path_without_ext",
+    "work_filename",
+    "idx",
+    "name",
+    "a_src",
+    "a_src_cut",
+    "a_enc_cut",
+    "_chapter",
+    "preset",
+    "clip",
+    "_trims_or_dfs",
+    "clip_cut",
+    "name_clip_output",
+    "name_file_final",
 ]
 
 
 def test_file_info_attr() -> None:
-    file = FileInfo('tests/video_file.mkv')
+    file = FileInfo("tests/video_file.mkv")
 
     assert len(vars(file)) == len(FILEINFO_ATTR)
 
